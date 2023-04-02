057b706fd5ddc6c8f7deebddb2588138da6c4a30
sams3arson <kirill.gemb@gmail.com>
2023-04-02 14:47:36 +0500

    __main__.py: adjust to __init__.py and import handlers

--- avitobot/__main__.py
+++ avitobot/__main__.py
@@ -1,107 +1,54 @@
 from pyrogram import Client, filters, idle
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup,\
-        CallbackQuery
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
-from pathlib import Path
-from transliterate import translit
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.job import Job
-from avitobot import database, texts, settings, wrappers, avito_api
-from avitobot.tools import creds
-from avitobot.states import State
-from avitobot.custom_types import UserId, CachedMarkup
 import asyncio
-import random
-import re
 
-credentials = creds.get(settings.CREDS_FILE)
-api_id, api_hash, bot_token, owner_id = credentials.api_id, credentials.api_hash,\
-        credentials.bot_token, credentials.owner_id
-
-db_conn = database.initialize()
-db_cursor = db_conn.cursor()
-
-allowed_users: list[UserId] = [owner_id] + database.get_allowed_users(db_conn)
-user_city: dict[UserId, str] = database.get_user_cities(db_conn)
-user_interval: dict[UserId, int] = database.get_user_intervals(db_conn)
-user_ping: set[UserId] = database.get_user_pings(db_conn)
-user_track_req: set[UserId] = database.get_user_track_reqs(db_conn)
-
-ping_job: dict[UserId, Job] = dict()
-track_req_job: dict[UserId, Job] = dict()
-
-user_states: dict[UserId, State] = dict()
-
-user_cached_markup: dict[UserId, CachedMarkup] = dict()
-
-scheduler = AsyncIOScheduler()
-avito = avito_api.Avito()
-
-def format_request_result(req_result: avito_api.RequestResult) -> str:
-    return texts.REQUEST_RESULT.format(min_price=req_result.min_price,
-               max_price=req_result.max_price, avg_price=req_result.avg_price,
-                           ads_count=req_result.ads_count, url=req_result.url)
-
-
-def start_ping_job(client: Client, user_id: UserId) -> Job:
-    return scheduler.add_job(send_ping, "interval", hours=4, args=(client, user_id))
-
-
-def start_pings(client: Client) -> None:
-    for user_id in user_ping:
-        ping_job[user_id] = start_ping_job(client, user_id)
-
-
-def start_track_req_job(client: Client, user_id: UserId):
-    minutes = user_interval.get(user_id)
-    if not minutes:
-        minutes = settings.DEFAULT_INTERVAL
-    return scheduler.add_job(track_request, "interval", minutes=minutes, args=(
-                                                            client, user_id))
-
-
-def start_track_requests(client: Client):
-    for user_id in user_track_req:
-        track_req_job[user_id] = start_track_req_job(client, user_id)
-
-
-async def send_ping(client: Client, user_id: UserId) -> None:
-    await client.send_message(user_id, "Бот жив.")
+from avitobot.states import State
+from avitobot.services import jobs
+from avitobot import (
+    avito,
+    settings,
+    wrappers,
+    handlers,
+    scheduler,
+    API_ID,
+    API_HASH,
+    BOT_TOKEN,
+    allowed_users,
+    user_states
+)
 
 
 async def main():
     await avito.setup_browser()
 
-    app = Client("avitobot", ***REMOVED***api_id, ***REMOVED***api_hash, ***REMOVED***bot_token)
+    app = Client("avitobot", ***REMOVED***API_ID, ***REMOVED***API_HASH, ***REMOVED***BOT_TOKEN)
 
-    start_pings(app)
-    start_track_requests(app)
+    jobs.start_pings(app)
+    jobs.start_track_requests(app)
 
-    app.add_handler(MessageHandler(ignore, ~filters.private | 
+    app.add_handler(MessageHandler(handlers.ignore, ~filters.private |
                                    ~filters.user(allowed_users)))
-    app.add_handler(MessageHandler(start, filters.command(["help", "start"])))
-    app.add_handler(MessageHandler(city, filters.command(["city"])))
-    app.add_handler(MessageHandler(interval, filters.command(["interval"])))
-    app.add_handler(MessageHandler(status, filters.command(["status"])))
-    app.add_handler(MessageHandler(ping, filters.command(["ping"])))
-    app.add_handler(MessageHandler(request, filters.command(["request"])))
-    app.add_handler(MessageHandler(stop, filters.command(["stop"])))
+    app.add_handler(MessageHandler(handlers.start_and_help, filters.command(["help", "start"])))
+    app.add_handler(MessageHandler(handlers.city, filters.command(["city"])))
+    app.add_handler(MessageHandler(handlers.interval, filters.command(["interval"])))
+    app.add_handler(MessageHandler(handlers.status, filters.command(["status"])))
+    app.add_handler(MessageHandler(handlers.ping, filters.command(["ping"])))
+    app.add_handler(MessageHandler(handlers.request, filters.command(["request"])))
+    app.add_handler(MessageHandler(handlers.stop, filters.command(["stop"])))
 
-
-    app.add_handler(MessageHandler(process_request, filters.create(
+    app.add_handler(MessageHandler(handlers.process_request, filters.create(
         wrappers.filter_state_wrapper(State.INPUT_REQUEST, user_states))))
-    app.add_handler(MessageHandler(process_city, filters.create(
+    app.add_handler(MessageHandler(handlers.process_city, filters.create(
         wrappers.filter_state_wrapper(State.INPUT_CITY, user_states))))
-    app.add_handler(MessageHandler(process_interval, filters.create(
+    app.add_handler(MessageHandler(handlers.process_interval, filters.create(
         wrappers.filter_state_wrapper(State.INPUT_INTERVAL, user_states))))
 
-    app.add_handler(CallbackQueryHandler(enable_track_request, filters.create(
+    app.add_handler(CallbackQueryHandler(handlers.enable_track_request, filters.create(
         wrappers.filter_callback_wrapper(settings.TRACK_REQUEST_PATTERN))))
-    app.add_handler(CallbackQueryHandler(process_stop, filters.create(
+    app.add_handler(CallbackQueryHandler(handlers.process_stop, filters.create(
         wrappers.filter_callback_wrapper(settings.STOP_TRACK_REQUEST_PATTERN))))
 
-    app.add_handler(MessageHandler(any_message))
-
+    app.add_handler(MessageHandler(handlers.any_message))
 
     await app.start()
 
@@ -115,4 +62,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
